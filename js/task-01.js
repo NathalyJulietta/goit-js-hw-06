ff4fbdacb5e4aaaf39ec43e5e1eda49d6424b084
Nathaly <napanarvaz@gmail.com>
2023-05-05 06:09:28 +0100

    Homework 06 JS

---
+++ js/task-01.js
@@ -0,0 +1,40 @@
+/*<ul id="categories">
+  <li class="item">
+    <h2>Animals</h2>
+    <ul>
+      <li>Cat</li>
+      <li>Hamster</li>
+      <li>Horse</li>
+      <li>Parrot</li>
+    </ul>
+  </li>
+  <li class="item">
+    <h2>Products</h2>
+    <ul>
+      <li>Bread</li>
+      <li>Prasley</li>
+      <li>Cheese</li>
+    </ul>
+  </li>
+  <li class="item">
+    <h2>Technologies</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Node.js</li>
+    </ul>
+  </li>
+</ul>
+
+Number of categories: 3
+
+Category: Animals
+Elements: 4
+
+Category: Products
+Elements: 3
+
+Category: Technologies
+Elements: 5*/
